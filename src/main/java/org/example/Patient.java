a2c76e8a95f53aa34dbec06a13bfdaaa2d1c243b
d4xika <katja.daxi@gmail.com>
2024-11-28 14:54:06 +0100

    Attribute Patientenklasse GUI hinzufügen

---
+++ src/main/java/org/example/Patient.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Date;
+
+public class Patient {
+
+    private String firstName;
+    private String lastName;
+    private long svn;
+    private Date dateOfBirth;
+    private String gender;
+    private String street;
+    private int streetNumber;
+    private int postalCode;
+    private String city;
+    private String nationality;
+}
