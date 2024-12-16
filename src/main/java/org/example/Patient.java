12cd0965ceb5c1c79722d2066be0c21510fa3e85
d4xika <katja.daxi@gmail.com>
2024-12-16 14:30:33 +0100

    Getter & Setter eingefügt

--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 public class Patient {
 
+    /**
+     * Attribute des Patienten
+     */
     private String firstName;
     private String lastName;
     private long svn;
@@ -14,4 +17,88 @@ public class Patient {
     private int postalCode;
     private String city;
     private String nationality;
+
+    /**
+     * Getter & Setter Methoden
+     * @return liefert Attribute zurück
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSvn() {
+        return svn;
+    }
+
+    public void setSvn(long svn) {
+        this.svn = svn;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }
